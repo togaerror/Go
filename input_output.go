9ee5a668359f00f3935e50ffb39fa53ef267c601
togaerror <togaerror@gmail.com>
2017-08-29 08:06:48 +0900

    入力出力

---
+++ input_output.go
@@ -0,0 +1,52 @@
+package main
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	var n int
+
+	// 遅い
+	fmt.Scan(&n)
+	fmt.Println(n)
+
+	// 1行づつ読み込む
+	fmt.Println("Line")
+	s, t := nextLine(), nextLine()
+	fmt.Println(s)
+	fmt.Println(t)
+
+	// スペース区切りで読み込む Text
+	fmt.Println("Split Text")
+	line := nextLine()
+	lines := strings.Fields(line)
+	for i := 0; i < len(lines); i++ {
+		fmt.Println(lines[i])
+	}
+
+	// スペース区切りで読み込む Number
+	fmt.Println("Split Number")
+	num := nextLine()
+	nums := strings.Fields(num)
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(nums[i])
+	}
+	arr := make([]int, len(nums))
+	var total int = 0
+	for i:= 0; i < len(nums); i++ {
+		arr[i], _ = strconv.Atoi(nums[i])
+		total += arr[i]
+	}
+	fmt.Println(total)
+}
